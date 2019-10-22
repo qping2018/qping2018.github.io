e6d95e9e437af9a48bc3466a1a01c962ab4581f7
qping2018 <qping2018@gmail.com>
2019-10-22 12:29:12 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -29,7 +29,7 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  node_default: 'mysite',
+  node_default: 'cfworker',
   // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
